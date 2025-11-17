18b82943227810cd848eb5e09f25bd13bab7891a
malikpooran26 <malik.pooran26@compscihigh.org>
2025-11-17 20:16:29 +0000

    finished the api.js

--- api.js
+++ api.js
@@ -5,3 +5,20 @@ const apiKey = process.env.apiKey2
 // run your code with node api.js
 // add your apiKey to .env if your API needs one, otherwise ignore
 // Follow the checklist on the Performance Assessment Google Doc
+console.log("Welcome to the superhero chat bot!!!!")
+console.log("Pick a superhero by entering the specific ID list down below")
+console.log("263: Flash", "470: Moon Knight", "490: NightCrawler", "213: Deadpool")
+console.log("If you want other numbers follow this link: https://superheroapi.com/ids.html")
+
+const choice = prompt("Enter in the code for the superhero you want to see");
+
+console.log(`Fetching ${choice} images`);
+
+const url = `https://superheroapi.com/api/${apiKey}/${choice}/image `
+
+async function fetchHero() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.url)
+}
+fetchHero();
