dbf3ff764d9bcc9c965d59c649c9266f7e05ad16
malikpooran26 <malik.pooran26@compscihigh.org>
2025-11-18 17:13:41 -0500

    I finished

--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,29 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+console.log("Welcome to the ChatGPT API bot!!!")
+console.log("What can I help you with today?")
+
+const choice = prompt("Ask me any question")
+
+const url = "https://api.openai.com/v1/responses"
+
+async function AIFetcher(){
+    const options = {
+    method: 'POST',
+    headers: {
+        "Authorization": 'Bearer ' + apiKey,
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        "model": "gpt-4.1",
+        "input": choice
+    })
+  };
+  const response = await fetch("https://api.openai.com/v1/responses", options)
+
+  const data = await response.json()
+  console.log(data.output[0].content[0].text)
+}
+
+AIFetcher();
